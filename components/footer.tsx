a2597b0fd732452efa35fb6df41a60f25d92a02b
manhildwinarno <manhildwinarno@gmail.com>
2026-04-30 21:21:08 +0700

    feat: update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,63 @@
+import Image from "next/image";
+import { FaInstagram, FaTiktok, FaWhatsapp } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer>
-      <div className="bg-red-950 min-h-[185px]"></div>
+    <footer className="w-full bg-red-950 min-h-[185px] flex flex-col lg:flex-row justify-center lg:justify-around items-center text-white p-7 gap-3">
+      <div className="hidden lg:flex flex-col justify-center max-w-52">
+        <Image
+          src="/Logo_gachoor.png"
+          alt="Logo Mie Gachoor"
+          width={80}
+          height={80}
+        />
+        <p>
+          Sensasi Mie Hotplate yang{" "}
+          <span className="text-orange-600">Panasnya Pantang Padam!</span>
+        </p>
+      </div>
+      <div className="hidden lg:flex flex-col justify-center lg:self-start font-medium gap-3">
+        <p>PAGE</p>
+        <a className="text-gray-300" href="">
+          Home
+        </a>
+        <a className="text-gray-300" href="">
+          Menu
+        </a>
+        <a className="text-gray-300" href="">
+          About
+        </a>
+      </div>
+      <div className="hidden lg:flex flex-col justify-center self-start gap-3">
+        <p>SUPPORT</p>
+        <a className="text-gray-300" href="">
+          Privacy Policy
+        </a>
+        <a className="text-gray-300" href="">
+          Terms of Use
+        </a>
+      </div>
+      <div className="flex flex-col justify-center items-center lg:items-start lg:self-start gap-2">
+        <p>Connect with us!</p>
+        <p>+628128121</p>
+        <div className="flex items-center mt-1 mb-1 justify-between w-full lg:max-w-39">
+          <FaInstagram className="text-2xl" />
+          <FaTiktok className="text-2xl" />
+          <FaWhatsapp className="text-2xl" />
+        </div>
+        <p>Ikuti keseharian dan promo kami!</p>
+      </div>
+      <div className="flex flex-col justify-center items-center lg:self-start gap-2">
+        <div className="hidden lg:flex flex-col justify-center self-start gap-2">
+          <p>OpenHours</p>
+          <p>Senin-Kamis, Sabtu-Ahad</p>
+          <p>09:00-23:00 WIB</p>
+          <p>Tutup Hari Jum'at</p>
+        </div>
+        <p className="block text-gray-300 text-center lg:text-left">
+          &copy; 2026 Mie Gachoor Saung Budhe. Didesain oleh Manhillih
+        </p>
+      </div>
     </footer>
   );
 }
